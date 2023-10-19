d29ac5a665c841d45f829e66177db559694a0119
aaravss <104297694+aaravss@users.noreply.github.com>
2023-10-19 16:28:34 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -49,16 +49,6 @@ canvas = createCanvas(screen_width, screen_height-150);
 canvas.position(0,150);
 }
 
-function speak(){
-  var synth = window.speechSynthesis;
-
-  var utterThis = new SpeechSynthesisUtterance(speak_data);
-
-  synth.speak(utterThis);
-
-  speak_data = to_number + " apples have been drawn";
-}
-
 function draw() {
   if(draw_apple == "set")
   {
@@ -68,8 +58,19 @@ function draw() {
 image(apple, x, y, 50, 50);
     }
     document.getElementById("status").innerHTML = to_number + " apples have been drawn";
+      speak_data = to_number + " apples have been drawn";
     speak();
     draw_apple = "";    
   }
 }
+
+function speak(){
+  var synth = window.speechSynthesis;
+
+  var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+  synth.speak(utterThis);
+}
+
+
 
